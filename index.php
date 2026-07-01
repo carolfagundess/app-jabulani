d9988d091b4dabb79df7c92ab5e6282e13946a53
Carol Fagundes <carolinarftk@gmail.com>
2026-06-30 21:53:21 -0300

    Ajustes finais no index e EventoController

--- index.php
+++ index.php
@@ -1,34 +1,15 @@
 <?php
-<<<<<<< HEAD
 session_start(); 
-
-require_once 'src/controllers/UsuarioController.php';
-require_once 'src/controllers/BasicoController.php';
-require_once 'src/controllers/EventoController.php';
-=======
-
-session_set_cookie_params([
-    'lifetime' => 0,
-    'path' => '/',
-    'domain' => '',
-    'secure' => true,
-    'httponly' => true,
-    'samesite' => 'Strict'
-]);
-session_start();
 
 require_once 'src/controller/UsuarioController.php';
 require_once 'src/controller/BasicoController.php';
 require_once 'src/controller/EventoController.php';
-require_once 'src/controller/AdminController.php';
->>>>>>> ccac9c18fec9b9ce00c6db49189a2420f096f412
 
 echo "<pre>";
 
 $requisicao = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); 
 echo $requisicao;
 
-<<<<<<< HEAD
 $aux = '/app-jabulani/';
 switch ($requisicao) {
     case $aux . 'login':
@@ -44,48 +25,22 @@ switch ($requisicao) {
         UsuarioController::salvarUsuario();
         break;
     case $aux . 'principal':
-=======
-// Roteamento
-$aux = '/app-jabulani/';
-switch ($requisicao) {
-
-    // Rotas de Usuário 
-    case $aux.'login':  
-        UsuarioController::formLogin();
-        break;
-    case $aux.'autenticar':  
-        UsuarioController::autenticar();
-        break;
-    case $aux.'logout':  
-        UsuarioController::logout();
-        break;
-    case $aux.'cadastro':  
-        UsuarioController::formCadastro();
-        break;
-    case $aux.'salvarUsuario':  
-        UsuarioController::salvarUsuario();
-        break;
-
-    // Rotas de Sistema e Eventos
-    case $aux.'principal':  
->>>>>>> ccac9c18fec9b9ce00c6db49189a2420f096f412
         BasicoController::principal();
         break;
     case $aux . 'listarEventos':
-        EventosController::listarEventos();
+        EventoController::listarEventos();
         break;
     case $aux . 'formInserirEvento':
-        EventosController::formInserirEvento();
+        EventoController::formInserirEvento();
         break;
     case $aux . 'inserirEvento':
-        EventosController::inserirEvento();
+        EventoController::inserirEvento();
         break;
-<<<<<<< HEAD
     case $aux . 'inscreverEvento':
-        EventosController::inscreverEvento();
+        EventoController::inscreverEvento();
         break;
     case $aux . 'meusEventos':
-        EventosController::meusEventos();
+        EventoController::meusEventos();
         break;
     case $aux . 'editarPerfil':
         UsuarioController::formEditarPerfil();
@@ -94,16 +49,16 @@ switch ($requisicao) {
         UsuarioController::salvarPerfil();
         break;
     case $aux . 'alterarEvento':
-        EventosController::alterarEvento();
+        EventoController::alterarEvento();
         break;
     case $aux . 'atualizarEvento':
-        EventosController::salvarEvento();
+        EventoController::salvarEvento();
         break;
     case $aux . 'excluirEvento':
-        EventosController::excluirEvento();
+        EventoController::excluirEvento();
         break;
     case '/api/eventos/lista':
-        EventosController::listarEventosAPI();
+        EventoController::listarEventosAPI();
         break;
     case '/api/usuarios/lista':
         UsuarioController::listarUsuariosAPI();
@@ -111,32 +66,6 @@ switch ($requisicao) {
     case $aux . 'excluirUsuario':
         UsuarioController::excluirUsuario();
         break;
-=======
-    case $aux.'alterarEvento':
-        EventosController::alterarEvento();
-        break;
-    case $aux.'atualizarEvento':
-        EventosController::salvarEvento();
-        break;
-    case $aux.'excluirEvento':
-        EventosController::excluirEvento();
-        break;
-        case $aux.'meusEventos':
-        EventosController::meusEventos();
-        break;
-    case $aux.'excluirUsuario':
-        UsuarioController::excluirUsuario();
-        break;
-    case $aux.'api/eventos':
-        EventosController::listarEventosAPI();
-        break;
-    case $aux.'api/usuarios':
-        UsuarioController::listarUsuariosAPI();
-        break;
-    case $aux.'inscrever':
-        EventosController::inscrever();
-        break;
->>>>>>> ccac9c18fec9b9ce00c6db49189a2420f096f412
     default:
         echo "Página não encontrada!";
         break;
--- src/controller/EventoController.php
+++ src/controller/EventoController.php
@@ -128,25 +128,6 @@ class EventosController
         }
     }
 
-<<<<<<< HEAD:src/controllers/EventoController.php
-    public static function inscreverEvento(): void
-    {
-        if (!isset($_SESSION['usuario_id'])) {
-            header('Location: /app-jabulani/login');
-            exit;
-        }
-        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id_evento'])) {
-            require_once 'src/DAO/UsuarioEventoDAO.php';
-            $dao = new UsuarioEventoDAO();
-            $dao->inscrever($_SESSION['usuario_id'], (int) $_POST['id_evento']);
-
-            header('Location: /app-jabulani/meusEventos');
-            exit;
-        }
-    }
-
-=======
->>>>>>> ccac9c18fec9b9ce00c6db49189a2420f096f412:src/controller/EventoController.php
     public static function meusEventos(): void
     {
         if (!isset($_SESSION['usuario_id'])) {
@@ -170,26 +151,4 @@ class EventosController
         echo json_encode($eventos);
         exit;
     }
-<<<<<<< HEAD:src/controllers/EventoController.php
-=======
-    public static function inscrever(): void
-    {
-        if (!isset($_SESSION['usuario_id'])) {
-            header('Location: /app-jabulani/login');
-            exit;
-        }
-        
-        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['idEvento'])) {
-            $idEvento = (int) trim($_POST['idEvento']);
-            $idUsuario = $_SESSION['usuario_id'];
-
-            require_once 'src/DAO/UsuarioEventoDAO.php';
-            $dao = new UsuarioEventoDAO();
-            $dao->inscrever($idUsuario, $idEvento);
-
-            header('Location: /app-jabulani/meusEventos');
-            exit;
-        }
-    }
->>>>>>> ccac9c18fec9b9ce00c6db49189a2420f096f412:src/controller/EventoController.php
 }
